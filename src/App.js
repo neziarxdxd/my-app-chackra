5aad479fd46414a2a01439b2deb3bb93797f50c0
Raizen Sangalang <rickdavidsangalang@gmail.com>
2022-03-27 21:50:14 +0800

    feature test only

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import {
   Code,
   Grid,
   theme,
+
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {AirbnbExample} from './AirbnbExample';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
+        
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
+         
+          <Text fontSize='6xl' fontWeight='900'>Rai Sangalang</Text>   
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
             </Text>
+            <AirbnbExample/>
             <Link
               color="teal.500"
               href="https://chakra-ui.com"
